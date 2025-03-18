fd46cdba4f05c015b0bc672ff30d1105dbd6684b
HoangLongEpam <longhnnguyen256@gmail.com>
2025-03-18 22:26:08 +0700

    fix: add tailwind config in order to scanning vue file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxtjs/apollo"],
+  css: ["~/assets/css/tailwind.css"],
   apollo: {
     clients: {
       default: {
